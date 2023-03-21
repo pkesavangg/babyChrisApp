9d4343204d50093e59d9e7566da1deddb85641f0
Kesavan Panchabakesan <pkesavan@greatergoods.com>
2023-03-21 10:54:15 +0530

    Initial Commit

---
+++ BabyChris/BabyChrisApp.swift
@@ -0,0 +1,20 @@
+//
+//  BabyChrisApp.swift
+//  BabyChris
+//
+//  Created by Kesavan Panchabakesan on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BabyChrisApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
