70328027f50f6eafd21a5190fc3f12ab452bae3e
Kesavan Panchabakesan <pkesavan@greatergoods.com>
2024-04-14 10:01:50 +0530

    Added AppDelegate file

---
+++ BabyChris/AppDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.swift
+//  BabyChris
+//
+//  Created by Kesavan Panchabakesan on 06/04/24.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+import FirebaseCore
+import FirebaseFirestore
+// no changes in your AppDelegate class
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
+        print(">> your code here !!")
+        return true
+    }
+}
+
+
+
+@main
+struct Testing_SwiftUI2App: App {
+
+    // inject into SwiftUI life-cycle via adaptor !!!
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView().environmentObject(GameService())
+        }
+    }
+}
